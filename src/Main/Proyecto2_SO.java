674bf39bd75ac0af97ac1512d59ef69cd4502ce7
Justdg74 <ddigiosia@correo.unimet.edu.ve>
2024-10-27 20:08:42 -0400

    Interfaz + Imagenes

---
+++ src/Main/Proyecto2_SO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+import Interfaz.Interfaz;
+
+/**
+ *
+ * @author digio
+ */
+public class Proyecto2_SO {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+        // TODO code application logic here
+        Interfaz ventana= new Interfaz();
+        ventana.setVisible(true);
+    }
+    
+}
